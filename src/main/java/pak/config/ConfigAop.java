b84a3551ebf5b8205826cd252312688bbc87b968
Nodar Sakvarelidze <nsakvarelidze12@gmail.com>
2019-05-30 01:16:03 +0400

    message

--- src/main/java/pak/config/ConfigAop.java
+++ src/main/java/pak/config/ConfigAop.java
@@ -1,6 +1,7 @@
 package pak.config;
 
 import org.springframework.aop.aspectj.annotation.AnnotationAwareAspectJAutoProxyCreator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -13,13 +14,18 @@ import pak.aop.MyBean3;
 @ComponentScan(basePackages = {"pak.aop"})
 public class ConfigAop {
 
+    @Autowired
+    MyBean3 myBean3;
+
     @Bean(initMethod = "init", destroyMethod = "destroy")
     @Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON)
     //@Lazy
-    public MyBean myBean() {
+    public MyBean myBean(MyBean3 myBn3) {
         MyBean mb = new MyBean();
-        //not DI
+        // not BEAN
         //mb.setOtherBean(new MyBean3());
+        mb.setOtherBean(myBn3);
+        mb.setOtherBean(myBean3);
         mb.setOtherBean(myBean3());
         return mb;
     }
